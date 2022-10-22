0a70254cac0c8a57f0e001f09e8ab373d397b703
Kathy Bartholomew <bruinkathy@gmail.com>
2022-10-22 13:36:02 -0700

    Create ICoverTypeRepository and CoverTypeRepository and Update IUnitOfWork and UnitOfWork

---
+++ myBookCandy.DataAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using myBookCandy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myBookCandy.DataAccess.Repository.IRepository
+{
+	public interface ICoverTypeRepository : IRepository<CoverType>
+	{
+		void Update(CoverType obj);
+	}
+}
