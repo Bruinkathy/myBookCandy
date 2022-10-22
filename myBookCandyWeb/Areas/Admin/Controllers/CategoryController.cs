47cae3f7f621a96e5b8c5ae68785e5e12fc005d4
Kathy Bartholomew <bruinkathy@gmail.com>
2022-10-22 13:11:57 -0700

    Create Category Controller and Views for Index, Create, Edit, and Delete

---
+++ myBookCandyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using myBookCandy.DataAccess.Repository.IRepository;
+using myBookCandy.Models;
+
+namespace myBookCandyWeb.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _unitOfWork.Category.GetAll();
+            return View(objCategoryList);
+        }
+
+
+        // CREATE -- GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // CREATE -- POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "~ Category Created Successfully ~";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        // EDIT -- GET
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+
+            if (categoryFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDbFirst);
+        }
+
+        // EDIT -- POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "~ Category Updated Successfully ~";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        // DELETE -- GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+
+            if (categoryFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDbFirst);
+        }
+
+        // DELETE -- POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "~ Category Deleted Successfully ~";
+            return RedirectToAction("Index");
+        }
+    }
+}
