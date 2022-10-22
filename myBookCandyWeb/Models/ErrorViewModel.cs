00e75276dfee4d0f097ba4ca5480d772c43d3238
Kathy Bartholomew <bruinkathy@gmail.com>
2022-10-21 17:55:18 -0700

    Add project files.

---
+++ myBookCandyWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace myBookCandyWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
